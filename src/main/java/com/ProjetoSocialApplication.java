17a60b9ec76a087926fb23c7a9c9f7b93366da46
Álvaro Bianor <bianor.alvaro12@gmail.com>
2019-06-17 21:30:07 -0300

    criando projeto

---
+++ src/main/java/com/ProjetoSocialApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSocialApplication.class, args);
+	}
+
+}
